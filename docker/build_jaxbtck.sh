a943ab8df0343b9b713de703dccd9e697a2d325e
Ankit <ankit.ku.jain@oracle.com>
2019-04-06 23:24:25 +0530

    added sigtestdev and javatest jars in the repo.modified build_jaxbtck.sh

--- docker/build_jaxbtck.sh
+++ docker/build_jaxbtck.sh
@@ -51,7 +51,13 @@ if [ -z "$GF_BUNDLE_URL" ]; then
   echo "Using default url for GF bundle: $DEFAULT_GF_BUNDLE_URL"
   export GF_BUNDLE_URL=$DEFAULT_GF_BUNDLE_URL
 fi
+export TCK_ROOT=$WORKSPACE
 #wget $WGET_PROPS $GF_BUNDLE_URL -O latest-glassfish.zip
+#getting jaxb-ri which is needed to build the JAXB TCK
+wget $WGET_PROPS http://central.maven.org/maven2/com/sun/xml/bind/jaxb-ri/2.3.2/jaxb-ri-2.3.2.zip -O jaxb-ri.zip
+unzip -o jaxb-ri.zip
+wget $WGET_PROPS https://repository.ow2.org/nexus/service/local/repositories/snapshots/content/org/ow2/asm/asm-commons/7.0-SNAPSHOT/asm-commons-7.0-20181027.133601-5.jar -O asm-commons-7.0.jar
+wget $WGET_PROPS https://repository.ow2.org/nexus/service/local/repositories/snapshots/content/org/ow2/asm/asm/7.0-SNAPSHOT/asm-7.0-20181027.133552-5.jar -O asm-7.0.jar
 #unzip -o latest-glassfish.zip
 #ls -l $GF_HOME/glassfish5/glassfish/
 
@@ -64,7 +70,52 @@ make -version
 which ksh
 ksh -version
 which awk
-awk -version
+#awk -version
+
+export TCK_ROOT=$BASEDIR
+sed -i s#^AWK*=*.*#AWK=/usr/bin/awk#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^BASENAME*=*.*#BASENAME=/usr/bin/basename#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^CHMOD*=*.*#CHMOD=/usr/bin/chmod#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^CAT*=*.*#CAT=/usr/bin/cat#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^CP*=*.*#CP=/usr/bin/CP#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^DATE*=*.*#DATE=/usr/bin/date#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^DIRNAME*=*.*#DIRNAME=/usr/bin/dirname#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^EGREP*=*.*#EGREP=/usr/bin/egrep#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^FIND*=*.*#FIND=/usr/bin/find#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^GREP*=*.*#GREP=/usr/bin/grep#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^KSH*=*.*#KSH=/usr/bin/ksh#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^LS*=*.*#LS=/usr/bin/ls#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^MKDIR*=*.*#MKDIR=/usr/bin/mkdir#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^MV*=*.*#MV=/usr/bin/mv#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^NAWK*=*.*#NAWK=/usr/bin/awk#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^PRINTF*=*.*#PRINTF=/usr/bin/printf#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^PS*=*.*#PS=/usr/bin/ps#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^PWD*=*.*#PWD=/usr/bin/pwd#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^RM*=*.*#RM='/usr/bin/rm -rf'#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^SED*=*.*#SED=/usr/bin/sed#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^SORT*=*.*#SORT=/usr/bin/sort#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^SPLIT*=*.*#SPLIT=/usr/bin/split#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^TAR*=*.*#TAR=/usr/bin/tar#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^TEE*=*.*#TEE=/usr/bin/tee#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^TEST*=*.*#TEST=/usr/bin/test#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^TR*=*.*#TR=/usr/bin/tr#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^PERL*=*.*#PERL=/usr/bin/perl#g $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
+sed -i s#^ZIP*=*.*#ZIP=/usr/bin/zip#g $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
+sed -i s#^UNZIP*=*.*#UNZIP=/usr/bin/unzip#g $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
+
+sed -i s#^GENERAL_JAVAHOME*=*.*#GENERAL_JAVAHOME=/opt/jdk1.8.0_191#g $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
+sed -i s#^PWD*=*.*#PWD=/usr/bin/pwd#g $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
+sed -i s#^JAXB_HOME*=*.*#JAXB_HOME=/home/jenkins/workspace/jaxb-tck_master/jaxb-ri#g $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
+sed -i s#^JAXB_20_RI_HOME*=*.*#JAXB_20_RI_HOME=/home/jenkins/workspace/jaxb-tck_master/jaxb-ri#g $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
+sed -i s#^JAVAHOME_6*=*.*#JAVAHOME_6=/opt/jdk1.8.0_191#g $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
+sed -i s#^SIGTEST_DIST*=*.*#SIGTEST_DIST=/home/jenkins/workspace/jaxb-tck_master/jaxb-tck#g $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
+
+sed -i s#^JAVATEST_JAR_LOC*=*.*#JAVATEST_JAR_LOC=/home/jenkins/workspace/jaxb-tck_master/jaxb-tck/lib#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^SIGTESTDEV_JAR_LOC*=*.*#SIGTESTDEV_JAR_LOC=/home/jenkins/workspace/jaxb-tck_master/jaxb-tck/lib#g $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i s#^ASM_JAR_LOCATION*=*.*#ASM_JAR_LOCATION=/home/jenkins/workspace/jaxb-tck_master#g $WORKSPACE/jaxb-tck/build/Defs.mk
+
+make REPOSITORIES=$TCK_ROOT/xml_schema clean
+make REPOSITORIES=$TCK_ROOT/xml_schema nightly
 
 echo "jaxb-tck is coming soon" > filename.txt
 zip -r jaxb-tck-2.3_latest.zip filename.txt
