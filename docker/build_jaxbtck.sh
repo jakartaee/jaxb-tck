1ea7d6be749109689367f1f57d9edc824a5184b9
Tomas Kraus <Tomas.Kraus@oracle.com>
2020-03-27 14:23:26 +0100

    Fixed build script.
    Signed-off-by: Tomas Kraus <Tomas.Kraus@oracle.com>

--- docker/build_jaxbtck.sh
+++ docker/build_jaxbtck.sh
@@ -110,15 +110,22 @@ sed -i 's#^UNZIP\s*=\s*.*#UNZIP=/usr/bin/unzip#g' $WORKSPACE/jaxb-tck/build/Defs
 
 sed -i 's#^GENERAL_JAVAHOME\s*=\s*.*#GENERAL_JAVAHOME=/opt/jdk1.8.0_191#g' $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
 sed -i 's#^PWD\s*=\s*.*#PWD=/usr/bin/pwd#g' $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
-sed -i 's#^JAXB_HOME\s*=\s*.*#JAXB_HOME = /home/jenkins/workspace/agent/jaxb-tck_master/jaxb-ri#g' $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
-sed -i 's#^JAXB_20_RI_HOME\s*=\s*.*#JAXB_20_RI_HOME = /home/jenkins/workspace/agent/jaxb-tck_master/jaxb-ri#g' $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
+sed -i 's#^JAXB_HOME\s*=\s*.*#JAXB_HOME = '"${WORKSPACE}"'/jaxb-ri#g' $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
+sed -i 's#^JAXB_20_RI_HOME\s*=\s*.*#JAXB_20_RI_HOME = '"${WORKSPACE}"'/jaxb-ri#g' $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
 sed -i 's#^JAVAHOME_6\s*=\s*.*#JAVAHOME_6 = /opt/jdk1.8.0_191#g' $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
-sed -i 's#^SIGTEST_DIST\s*=\s*.*#SIGTEST_DIST = /home/jenkins/workspace/agent/jaxb-tck_master/jaxb-tck#g' $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
+sed -i 's#^SIGTEST_DIST\s*=\s*.*#SIGTEST_DIST = '"${WORKSPACE}"'/jaxb-tck#g' $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
 sed -i 's#^ANT_HOME\s*=\s*.*#ANT_HOME = /usr/share/ant#g' $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
 
-sed -i 's#^JAVATEST_JAR_LOC\s*=\s*.*#JAVATEST_JAR_LOC = /home/jenkins/workspace/agent/jaxb-tck_master/jaxb-tck/lib#g' $WORKSPACE/jaxb-tck/build/Defs.mk
-sed -i 's#^SIGTESTDEV_JAR_LOC\s*=\s*.*#SIGTESTDEV_JAR_LOC = /home/jenkins/workspace/agent/jaxb-tck_master/jaxb-tck/lib#g' $WORKSPACE/jaxb-tck/build/Defs.mk
-sed -i 's#^ASM_JAR_LOCATION\s*=\s*.*#ASM_JAR_LOCATION = /home/jenkins/workspace/agent/jaxb-tck_master#g' $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i 's#^JAVATEST_JAR_LOC\s*=\s*.*#JAVATEST_JAR_LOC = '"${WORKSPACE}"'/jaxb-tck/lib#g' $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i 's#^SIGTESTDEV_JAR_LOC\s*=\s*.*#SIGTESTDEV_JAR_LOC = '"${WORKSPACE}"'/jaxb-tck/lib#g' $WORKSPACE/jaxb-tck/build/Defs.mk
+sed -i 's#^ASM_JAR_LOCATION\s*=\s*.*#ASM_JAR_LOCATION = '"${WORKSPACE}"'#g' $WORKSPACE/jaxb-tck/build/Defs.mk
+
+echo '-[ Defs.mk ]----------------------------------------------------------'
+cat $WORKSPACE/jaxb-tck/build/Defs.mk
+echo '----------------------------------------------------------------------'
+echo '-[ Defs.SFBay.mk ]----------------------------------------------------'
+cat $WORKSPACE/jaxb-tck/build/Defs.SFBay.mk
+echo '----------------------------------------------------------------------'
 
 cd $TCK_ROOT/jaxb-tck/build
 
