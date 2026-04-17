92d6591a4a5c5afd71f9a2aca444f28ad390f35a
Lukas Jungmann <1069133+lukasj@users.noreply.github.com>
2026-04-17 21:12:18 +0200

    update license before src move

--- jaxb-tck/src/share/jttck/classes/com/sun/jaxb_tck/interview/moreInfo/tckwiz.hs
+++ jaxb-tck/src/share/jttck/classes/com/sun/jaxb_tck/interview/moreInfo/tckwiz.hs
@@ -2,11 +2,12 @@
 <!DOCTYPE helpset  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"  "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
 <!--
 
+    Copyright (c) 2026 Contributors to the Eclipse Foundation.
     Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
 
     This program and the accompanying materials are made available under the
     terms of the Eclipse Public License v. 2.0, which is available at
-    http://www.eclipse.org/legal/epl-2.0.
+    https://www.eclipse.org/legal/epl-2.0.
 
     This Source Code may also be made available under the following Secondary
     Licenses when the conditions for such availability set forth in the
