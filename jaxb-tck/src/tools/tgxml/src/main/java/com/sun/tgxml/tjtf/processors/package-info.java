6db17772957a3b8e6c973d648b3c042f87c6aa7d
Lukas Jungmann <1069133+lukasj@users.noreply.github.com>
2026-05-14 21:09:29 +0200

    #110: move package.html to package-infos

---
+++ jaxb-tck/src/tools/tgxml/src/main/java/com/sun/tgxml/tjtf/processors/package-info.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * https://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+/**
+ * The <em>Processors</em> package provides the interfaces that represent <b><em>Processor</em></b> components that a
+ * tool may own. These <em>Processors</em> classes generally support IO functions for a specific Internal Representation
+ * (such as parsing, validating, emitting).
+ * <p>
+ *
+ * @since 10/25/2001
+ */
+package com.sun.tgxml.tjtf.processors;
